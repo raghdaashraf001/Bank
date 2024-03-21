89c0428956b72a1975e9ac76999da6d22ca2e405
raghdaashraf001 <raghdaashraf001@gmail.com>
2024-03-21 05:15:39 +0200

    first

---
+++ include/Person.h
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+//Person Class:
+
+class Person {
+protected:
+	//Attributes:
+	string name;
+    int id;
+	string password;
+public:
+
+	//constructors:
+	Person() {
+	}
+	Person(string name,int id, string password) {
+        this->name=name;
+        this->id=id;
+        this->password=password;
+
+	}
+
+	//setters
+	void setName(string name) {
+        this->name=name;
+	}
+	void setId(int id) {
+        this->id=id;
+	}
+    void setPassword(string password) {
+        this->password=password;
+	}
+	//Getters:
+	string getName() {
+		return name;
+	}
+	int getId(){
+        return id;
+	}
+	string getPassword() {
+		return password;
+	}
+
+	//Methods:
+    string printInfo() {
+		cout << "Name : " << name << endl;
+		cout << "ID   : " << id << endl;
+
+	}
+
+	//Destructor
+	~Person() {
+
+	}
+};
